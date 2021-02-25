cdcf2d182c623f106026af22fd963ad2aa9c0c86
Skyascii <38729705+savioxavier@users.noreply.github.com>
2021-02-25 21:02:32 +0400

    Update leetspeak.js

--- src/leetspeak.js
+++ src/leetspeak.js
@@ -5,10 +5,9 @@ function convertToLeet() {
   // User input string
   var str = document.querySelector("#regular-text").value;
 
-  // System output string
   var result = document.querySelector("#result");
 
-  var type = document.getElementById("leet-type").value;
+  var type = document.querySelector("#leet-type").value;
 
   var chars = {};
 
@@ -166,7 +165,20 @@ tname.textContent = `< ${theme} >`;
 
 tname.addEventListener("click", changeTheme);
 
-// You know the rules and so I
+// Since select() [the standard for copying text] can only be used for visible input elements such as textarea, we extract the text content of <pre> element and assign it to a new <textarea> element. Then we proceed to copy the content of the "invisible" textarea.
+
+function copyText() {
+  var preText = document.querySelector("#result").textContent;
+  var copyTextbox = document.createElement("textarea");
+  copyTextbox.textContent = preText;
+  document.body.append(copyTextbox);
+  copyTextbox.classList.add("copy-textbox");
+  copyTextbox.select();
+  document.execCommand("copy");
+  alert("The text has been copied!");
+}
+
+// You know the rules and so do I
 // I know it's annoying, but I really had to do it
 
 function getRickrolled() {
@@ -174,8 +186,9 @@ function getRickrolled() {
     "W3'r3 n0 5tr4ng3r5 t0 l0v3\nY0u kn0w th3 rul35 4nd 50 d0 I\nA full c0mm1tm3nt'5 wh4t I'm th1nk1ng 0f\nY0u w0uldn't g3t th15 fr0m 4ny 0th3r guy\n\nI ju5t w4nn4 t3ll y0u h0w I'm f33l1ng\nG0tt4 m4k3 y0u und3r5t4nd\n\nN3v3r g0nn4 g1v3 y0u up\nN3v3r g0nn4 l3t y0u d0wn\nN3v3r g0nn4 run 4r0und 4nd d353rt y0u\nN3v3r g0nn4 m4k3 y0u cry\nN3v3r g0nn4 54y g00dby3\nN3v3r g0nn4 t3ll 4 l13 4nd hurt y0u\n\nW3'v3 kn0wn 34ch 0th3r f0r 50 l0ng\nY0ur h34rt'5 b33n 4ch1ng but y0u'r3 t00 5hy t0 54y 1t\nIn51d3 w3 b0th kn0w wh4t'5 b33n g01ng 0n\nW3 kn0w th3 g4m3 4nd w3'r3 g0nn4 pl4y 1t\n\nAnd 1f y0u 45k m3 h0w I'm f33l1ng\nD0n't t3ll m3 y0u'r3 t00 bl1nd t0 533\n\nN3v3r g0nn4 g1v3 y0u up\nN3v3r g0nn4 l3t y0u d0wn\nN3v3r g0nn4 run 4r0und 4nd d353rt y0u\nN3v3r g0nn4 m4k3 y0u cry\nN3v3r g0nn4 54y g00dby3\nN3v3r g0nn4 t3ll 4 l13 4nd hurt y0u\n\nN0, I'm n3v3r g0nn4 g1v3 y0u up\nN0, I'm n3v3r g0nn4 l3t y0u d0wn\nN0, I'll n3v3r run 4r0und 4nd hurt y0u\nN3v3r, 3v3r d353rt y0u\n\nW3'v3 kn0wn 34ch 0th3r f0r 50 l0ng\nY0ur h34rt'5 b33n 4ch1ng but\n\nN3v3r g0nn4 g1v3 y0u up\nN3v3r g0nn4 l3t y0u d0wn\nN3v3r g0nn4 run 4r0und 4nd d353rt y0u\nN3v3r g0nn4 m4k3 y0u cry\nN3v3r g0nn4 54y g00dby3\nN3v3r g0nn4 t3ll 4 l13 4nd hurt y0u\n\nN0, I'm n3v3r g0nn4 g1v3 y0u up\nN0, I'm n3v3r g0nn4 l3t y0u d0wn\nN0, I'll n3v3r run 4r0und 4nd hurt y0u\nI'll n3v3r, 3v3r d353rt y0u";
 
   console.log(rickrollText);
-  console.log("Themes from Opera GX");
-  console.log("Intro from https://en.wikipedia.org/wiki/Leet");
 }
+
+console.log("Intro from https://en.wikipedia.org/wiki/Leet");
+console.log("Themes from Opera GX");
 
 getRickrolled();
